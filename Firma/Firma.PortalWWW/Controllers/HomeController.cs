83775be5ea33dedd9bb075f95b5dabfa167a4a6d
DziedzicFilip <dziedzicfilip00@gmail.com>
2025-04-27 20:15:26 +0200

    wyswietlanie promocjiw index

--- Firma/Firma.PortalWWW/Controllers/HomeController.cs
+++ Firma/Firma.PortalWWW/Controllers/HomeController.cs
@@ -1,41 +1,76 @@
 using System.Diagnostics;
+using AspNetCoreGeneratedDocument;
+using Firma.Data.Data;
+using Firma.Data.Data.CMS;
+using Firma.Data.Data.Sklep;
 using Firma.PortalWWW.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Firma.PortalWWW.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly FirmaContext _context;
+        public HomeController(ILogger<HomeController> logger,FirmaContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+        private async Task<Strona?> GetStronaWithModelStronyAsync(int? id)
+        {
+            
+            ViewBag.ModelStrony = (
+                from strona in _context.Strona
+                orderby strona.Pozycja
+                select strona
+            ).ToList();
+
+           
+            if (id == null)
+            {
+                id = 1;
+            }
+
+            
+            return await _context.Strona.FindAsync(id);
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index(int? id)
         {
-            return View();
+            ViewBag.ModelPromocji = _context.ProduktPromocja
+    .Include(pp => pp.Towar)  // pozwlaa na dolaczenie dancy z atabeli Towar
+    .Include(pp => pp.Promocja) // pozwlaa na do³aczenie danych z tabeli Promocja
+    .ToList();
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item);
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy(int? id)
         {
-            return View();
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item);
         }
-        public IActionResult Przeglad() {
-            return View();
+        public async Task<IActionResult> Przeglad(int? id) {
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item);
         }
-        public IActionResult Szczegoly()
+        public async Task<IActionResult> Szczegoly(int? id)
         {
-            return View();
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item);
         }
-        public IActionResult Kontakt()
+        public async Task<IActionResult> Kontakt(int? id)
         {
-            return View();
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item); ;
         }
-        public IActionResult Regulamin()
+        public async Task<IActionResult> Regulamin(int? id)
         {
-            return View();
+            var item = await GetStronaWithModelStronyAsync(id);
+            return View(item);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
